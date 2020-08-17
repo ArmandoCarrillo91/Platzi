e395ac800ef7f956c914d8f761ab68ece238064e
Armando Carrillo <jesus.carrillo@payclip.com>
2020-08-17 11:57:27 -0500

    removi los archivoq que no son del curso de git

--- Cursos/basic_python/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-dolares = input('Cuantos dolares tienes?: ')
-dolares = float(dolares)
-valor_dolar = 0.045
-peso = dolares / valor_dolar
-peso = round(peso,2)
-peso = str(peso)
-print('Tienes $' + peso + ' Pesos')
